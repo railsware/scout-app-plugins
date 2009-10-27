c552ade4089ba23e976afeb57f2bcf051b286f4b
Dmitry Larkin <dmitry.larkin@gmail.com>
2009-10-27 22:00:20 +0200

    http_connections fixes

--- http_connections/http_connections.rb
+++ http_connections/http_connections.rb
@@ -1,23 +1,29 @@
 class HttpConnections < Scout::Plugin
 
   def build_report
+
     data = {}
     %w( CLOSED LISTEN SYN_SENT SYN_RCVD ESTABLISHED CLOSE_WAIT LAST_ACK FIN_WAIT_1 FIN_WAIT_2 CLOSING TIME_WAIT ).each do |status|
       data.merge!({status.downcase.to_sym => netstat_count(status).strip})
     end
+
     report(data)
+
   rescue Exception => e
     error "Couldn't parse output. Make sure you have netstat installed. #{e}"
     logger.error e
   end
 
   private
-  
+
   def netstat_count(state)
     lambda do
-      port = option('port') || 80
-      command = option('command') || "netstat -an | grep #{state} | awk '{ print $4 }' | grep -e '[:\.]#{port}$' | wc -l"
-      `#{command}`
+      port    = option('port') || 80
+      netstat = option('netstat') || "netstat -an"
+      awk     = option('awk') || "awk '{ print $4 }'"
+      cmd     = "#{netstat} | grep #{state} | #{awk} | grep -e '[:\.]#{port}$' | wc -l "
+
+      `#{cmd}`
     end.call
   end
 
