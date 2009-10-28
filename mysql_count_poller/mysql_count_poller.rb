726bb89af61189327788b80ca1907b7bcae9d919
Dmitry Larkin <dmitry.larkin@gmail.com>
2009-10-28 17:26:49 +0200

    mysql count poller

---
+++ mysql_count_poller/mysql_count_poller.rb
@@ -0,0 +1,29 @@
+class MysqlCountPoller < Scout::Plugin
+  def build_report
+    string = query_output_string.split(/:/).last
+    string.strip!
+
+    report(:count => string)
+  rescue Exception => e
+    error "Couldn't parse output. Make sure you have proper SQL. #{e}"
+    logger.error e
+  end
+
+  private
+
+  def query_output_string
+    mysql     = option('mysql') || 'mysql'
+    host      = option('host') || '127.0.0.1'
+    user      = option('user') || 'root'
+    password  = option('password') || ''
+    query     = option('query') || 'SELECT 0;'
+
+    query.strip!
+    query.chomp!(';')
+
+    cmd = "#{mysql} --user='#{user}' --password='#{password}' --execute='#{query}\\G' | tail -n1"
+
+    `#{cmd}`
+  end
+
+end
