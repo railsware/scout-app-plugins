3b879f2e0d36be96b27aeea88ca2157c731c7567
Yaroslav Lazor <ylazor@railsware.com>
2009-10-29 17:12:12 -0400

    moved init away fixed if block typo

--- log_watcher/plugin.rb
+++ log_watcher/plugin.rb
@@ -1,6 +1,5 @@
 class LogWatcher < Scout::Plugin
-  
-  def build_report
+  def init
     @log_file_path = option("log_path").to_s.strip
     if log_file_path.empty?
       return error( "A path to the log file wasn't provided." )
@@ -14,34 +13,35 @@ class LogWatcher < Scout::Plugin
     end
 
     @error_pipe = option("error_pipe").to_s.strip
- 
+    nil
+  end
+  
+  def build_report
+    return if init()
+    
     last_run = memory(:last_run) || 0
     current_length = `wc -c #{@log_file_path}`.split(' ')[0].to_i
 
     # don't run it the first time
-    if (last_run > 0 ) { 
+    if (last_run > 0 )
       read_length = current_length - last_run
 
       value = `tail -c #{read_length} #{@log_file_path} | #{@value_pipe}`.strip
       report(:value => value, :error => error_pipe)
 
       errors = `tail -c #{read_length} #{@log_file_path} | #{@error_pipe}`.strip unless @error_pipe.empty?
-      unless errors.to_s.empty? {
+      unless errors.to_s.empty?
         alert(build_alert(errors))
-      }
-    }
+      end
+    end
     remember(:last_run, current_length)
   rescue Errno::ENOENT => error
-      error(error.to_s)    
+    error(error.to_s)    
   end
   
   def build_alert(errors, service)
     subj = "Receiving errors from the #{service}"
-    body = String.new
-    slow_queries.each do |sq|
-      body << errors
-      body << "\n\n"
-    end
+    body == errors+"\n\n"
     {:subject => subj, :body => body}
   end
   
