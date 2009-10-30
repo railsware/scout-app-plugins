dfbc93db478dcae7a661b731c4e7869e9ec61abc
Yaroslav Lazor <ylazor@railsware.com>
2009-10-30 19:15:30 -0400

    improved http_connection performance

--- http_connections/http_connections.rb
+++ http_connections/http_connections.rb
@@ -2,9 +2,10 @@ class HttpConnections < Scout::Plugin
 
   def build_report
 
+    response = netstat()
     data = {}
     %w( CLOSED LISTEN SYN_SENT SYN_RCVD ESTABLISHED CLOSE_WAIT LAST_ACK FIN_WAIT_1 FIN_WAIT_2 CLOSING TIME_WAIT ).each do |status|
-      data.merge!({status.downcase.to_sym => netstat_count(status).strip})
+      data.merge!({status.downcase.to_sym => netstat_count(response, status)})
     end
 
     report(data)
@@ -16,15 +17,19 @@ class HttpConnections < Scout::Plugin
 
   private
 
-  def netstat_count(state)
+  def netstat()
     lambda do
       port    = option('port') || 80
       netstat = option('netstat') || "netstat -an"
-      awk     = option('awk') || "awk '{ print $4 }'"
-      cmd     = "#{netstat} | grep #{state} | #{awk} | grep -e '[:\.]#{port}$' | wc -l "
+      awk     = option('awk') || "awk '{ print $4\" \"$6 }'"
+      sort    = option('sort') || "sort | uniq -c | awk '{print $1\" \"$3}'"
+      cmd     = "#{netstat} | #{awk} | grep -e '[:\.]#{port} ' | #{sort}"
 
       `#{cmd}`
     end.call
   end
-
+  
+  def netstat_count(response, state)
+    response[/(\d+) #{state}/,1].to_i
+  end
 end
