41a74f00a683ca193b43a35ec4939b582bc476d7
Dmitry Larkin <dmitry.larkin@gmail.com>
2010-02-01 17:27:12 +0200

    ehs_flow_statistics dependency fix

--- ehs_flow_statistics/plugin.rb
+++ ehs_flow_statistics/plugin.rb
@@ -1,14 +1,12 @@
-require 'redis'
-
 class EhsFlowStatistics < Scout::Plugin
-  needs 'rubygems', 'redis', 'rp-stats'
+  needs 'rp-stats'
 
   MEGABYTE = 1048576
 
   def build_report
-    RpStats.load({ :hosts=> option(:redis) })
+    RpStats.load({ :hosts => option("redis") })
 
-    stats = RpStats.flush(option(:namespace))
+    stats = RpStats.flush(option("namespace"))
     stats[:io] = stats[:io].to_f / MEGABYTE
 
     report(stats)
