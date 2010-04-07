8fdc3a76008a86c5a94ade480377947e0f6436f5
Serhiy Rozum <sergey.rozum@gmail.com>
2010-04-07 17:08:26 +0300

    Nginx monitoring works

--- nginx_report/plugin.rb
+++ nginx_report/plugin.rb
@@ -3,12 +3,10 @@ require 'open-uri'
 class NginxReport < Scout::Plugin
 
   def build_report  
-    url = option(:url) || 'http://127.0.0.1/nginx_status'
-
     total, requests, reading, writing, waiting = nil
-
-    current_time = Time.now.to_i
-    last_run = memory(:last_run)
+    url           = option(:url) || 'http://127.0.0.1/nginx_status'
+    last_run      = memory(:last_run) || Time.now.to_i
+    current_time  = Time.now.to_i
 
     open(url) {|f|
       f.each_line do |line|
@@ -23,13 +21,13 @@ class NginxReport < Scout::Plugin
           current_requests = $3.to_i 
           last_requests = (memory(:requests) || current_requests).to_i
           # handle nginx stats reset
-          current_requests = last_requests + current_requests if ( last_requests > current_requests )
-          remember(:requests, current_requests)
+          current_requests = (last_requests + current_requests) if ( last_requests > current_requests )
+          remember({:requests => current_requests})
           requests = (current_requests-last_requests)/(current_time-last_run).to_f
         end
       end
     }
-    remember(:last_run, current_time)
+    remember({:last_run => current_time})
   
     report({:total => total, :reading => reading, :writing => writing, :waiting => waiting, :requests => requests})
   end
