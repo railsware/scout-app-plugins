a05a545993b826c164cc128b384932cd38ac52c8
Alexander Lockshyn <info@lab37.com>
2011-08-26 19:41:23 +0300

    Options format changes

--- log_watcher.rb
+++ log_watcher.rb
@@ -40,7 +40,7 @@
 #    
 # Dependencies
 # ------------
-# Requires gems : [set, mysql]
+# Requires gems : [set]
 # 
 # Compatibility 
 # -------------
@@ -48,29 +48,22 @@
 
 class LogWatcher < Scout::Plugin
   OPTIONS=<<-EOS
-  options:
-    log_path:
-      default: /var/log/my.log
-      name: Log path
-      notes: Full path to the the log file
-    service_name:
-      default: MyService
-      name: Service name
-      notes: Name of the service - the owner of the log. Will be shown in the alert
-    value_pipe:
-      default: egrep "PottencialError" | egrep -v "Junk" | wc -l
-      name: Value Pipe
-      notes: A pipe command that goes right aftail tail #{log}
-    error_pipe:
-      default: egrep "PottencialError" | egrep -v "Junk" | sort | uniq -c | sort -nr
-      name: Error Pipe
-      notes: A pipe command that goes right aftail tail #{log} to aggregate the errors and send a notification over scout
-
-  metadata:
-    value:
-      unit: /min
-      precision: 2
-
+  log_path:
+    default: /var/log/my.log
+    name: Log path
+    notes: Full path to the the log file
+  service_name:
+    default: MyService
+    name: Service name
+    notes: Name of the service - the owner of the log. Will be shown in the alert
+  value_pipe:
+    default: egrep "PottencialError" | egrep -v "Junk" | wc -l
+    name: Value Pipe
+    notes: A pipe command that goes right aftail tail log
+  error_pipe:
+    default: egrep "PottencialError" | egrep -v "Junk" | sort | uniq -c | sort -nr
+    name: Error Pipe
+    notes: A pipe command that goes right aftail tail log to aggregate the errors and send a notification over scout
   EOS
 
   def init
--- mysql_query_count_poller.rb
+++ mysql_query_count_poller.rb
@@ -11,8 +11,30 @@
 # Compatibility 
 # -------------
 # 
-
 class MysqlQueryCountPoller < Scout::Plugin
+  OPTIONS=<<-EOS
+  mysql:
+    name: mysql
+    notes: mysql command or full path
+    default: mysql
+  host:
+    name: Host
+    notes: MySQL Host
+    default: 127.0.0.1
+  user:
+    name: User
+    notes: MySQL User
+    default: root
+  password:
+    name: Password
+    notes: MySQL Password
+    default: 
+  query:
+    name: mysql query
+    notes: MySQL Query with ; delimeter. Result should be digit
+    default: 'SELECT 0 as count;'
+  EOS
+  
   def build_report
     library_available? 'open3'    
     report(query_output)
@@ -25,11 +47,11 @@ class MysqlQueryCountPoller < Scout::Plugin
   private
 
   def query_output
-    mysql     = option('mysql')     || 'mysql'
-    host      = option('host')      || '127.0.0.1'
-    user      = option('user')      || 'root'
-    password  = option('password')  || ''
-    query     = option('query') || 'SELECT 0 as count;'
+    mysql     = option(:mysql)    || 'mysql'
+    host      = option(:host)     || '127.0.0.1'
+    user      = option(:user)     || 'root'
+    password  = option(:password) || ''
+    query     = option(:query) || 'SELECT 0 as count;'
 
     query.strip!
     query.chomp!(';')
